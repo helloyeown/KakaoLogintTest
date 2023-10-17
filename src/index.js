1b43474cedbd9c9185cbfd22b856fa0efde854c7
hiYewonMoon1 <toquasimodox@gmail.com>
2023-10-17 20:55:56 +0900

    kakao login

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import KakaoLoginComponent from './components/KakaoLoginComponent';
+import { Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <KakaoLoginComponent></KakaoLoginComponent>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
