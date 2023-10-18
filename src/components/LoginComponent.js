6a3c5ef583ee925d210c8daaa76bdc0f0ef56e5a
hiYewonMoon1 <toquasimodox@gmail.com>
2023-10-18 20:59:38 +0900

    kakao login test path error check

---
+++ src/components/LoginComponent.js
@@ -0,0 +1,10 @@
+
+const LoginComponent = () => {
+    return (  
+        <div>
+            <div className="text-3xl font-bold">Login</div>
+        </div>
+    );
+}
+ 
+export default LoginComponent;
